c2536fe764ddfad7d6024bda0c5efca42c4500cb
remiel14 <remielrelevo14@gmail.com>
2025-03-15 16:34:39 +0800

    Activity 2 part 1

--- Relevo_Remiel_Lab_2.cpp
+++ Relevo_Remiel_Lab_2.cpp
@@ -1,56 +1,49 @@
 #include <iostream>
-
 using namespace std;
 
 typedef struct Node{
-    string songName;
-    Node  *link;
+        string songName;
+        Node *link;
 }Node;
 
-Node *createNode(string data){
-    Node *newNode = new Node;
+Node* createNode(string data) {
+    Node* newNode = new Node;
     newNode->songName = data;
     newNode->link = NULL;
-
     return newNode;
 }
-
-void traverse(Node *head){
+void traverse (Node *head){
     Node *temp = new Node;
     temp = head;
     
     cout << "My Playlist" <<endl;
-    while(temp != NULL){
-        cout << temp->songName<< "->"<<endl;
+    while(temp !=NULL){
+        cout << temp->songName<< "->" <<endl;
         if(temp->link == NULL){
-            cout << "NULL"<<endl;
-        }
-        
-        temp = temp->link;
+            cout << "NULL" << endl;
     }
+    temp = temp->link;
 }
-
+}
 Node *insertAtEnd(string data, Node *head){
     if(head == NULL){
         Node *newNode = createNode(data);
         head = newNode;
-        cout << "A new node has been inserted at the end \n" << endl;
+        cout << "A new node has been insterted at the end \n" << endl;
         return head;
     }
     Node *temp = new Node;
     temp = head;
     
-    while(temp->link != NULL){
+    while (temp->link != NULL){
         temp = temp->link;
     }
-    
     Node *newNode = createNode(data);
     temp->link = newNode;
     
-    cout << "A new node has been inserted at the end \n" << endl;
+    cout << "A new node has been insterted at the end \n" << endl;
     return head;
 }
-
 Node *insertAtBeginning(string data, Node *head){
     Node *newNode = createNode(data);
     newNode->link = head;
@@ -58,17 +51,15 @@ Node *insertAtBeginning(string data, Node *head){
     head = newNode;
     
     cout << "A new node has been inserted at the beginning \n" <<endl;
-    
     return head;
 }
-
 string insertAfter(string after, string data, Node *head){
-    Node *temp = new Node;
-    temp = head;
+    Node *temp = new Node; 
+    temp = head; 
     
-    while(temp->songName.compare(after) != 0){
+        while(temp->songName.compare(after) != 0){
         if(temp == NULL){
-            return "No such exist, please try again later.";
+            return "No such song exist, please try again.";
         }
         
         temp = temp->link;
@@ -77,29 +68,97 @@ string insertAfter(string after, string data, Node *head){
     newNode->link = temp->link;
     temp->link = newNode;
     
-    return "An new node has been inserted after " + after + "\n";
+    return "An new node has been added after" + after + "\n";
 }
+string deleteAtEnd(Node *head){
+    if(head == NULL){ 
+        return "The linked list is empty \n";
+    }
 
-int main(){
-    Node *head = createNode("Sanctuary by Joji");
+    if(head->link == NULL){ 
+        delete head;
+        return "The head has been deleted \n";
+    }
+
+    Node *temp = new Node; 
+    temp = head; 
+
+    while(temp->link->link != NULL){ 
+        temp = temp->link;
+    }
+
+    temp->link = NULL; 
+
+    return "A node has been deleted at the end \n";
+}
+Node *deleteFromBeginning(Node *head){
+    if(head == NULL){
+        cout << "The linked list is empty \n" << endl;
+        return NULL;
+    }
     
-    head = insertAtEnd("BMF by SZA", head);
-    head = insertAtEnd("Snooze by SZA", head);
-    head = insertAtEnd("Lucid Dreams by Juice WRLD", head);
-    head = insertAtEnd("Again by fettywap", head);
+    if(head->link == NULL){
+        delete head;
+    }
+    head = head->link;
     
-    head = insertAtBeginning("Gods plan by Drake", head);
-    head = insertAtBeginning("City Girl by Shantidope", head);
-    head = insertAtBeginning("All Girls Are The Same by Juice WRLD", head);
-    head = insertAtBeginning("AMAZING by Rex Orange Country", head);
+    cout << "A node has been delete from the beginning \n" << endl;
+    
+    return head;
+}
+Node *deleteFromGivenNode(string givenNode, Node *head){
+    if(head == NULL){
+        cout << "The Node" + givenNode + " has been deleted. \n" << endl;
+        return head;
+    }
+    Node *temp = new Node;
+    Node *next = new Node;
+    temp = head;
+    next = temp->link;
+    
+    while(next->songName.compare(givenNode) !=0){
+        if(temp == NULL){
+            cout << "No Such node exist. \n" << endl;
+            return head;
+    }
+    next = next->link;
+    temp = temp->link;
+}
+    temp->link = next->link;
+    cout << "The Node " + givenNode + " has been deleted. \n" << endl;
+    return head;
+}
+int main()
+{
+    Node *head = createNode ("Snooze by SZA");
+    traverse(head);
+    head = insertAtEnd("All Girls Are The Same By Juice wrld", head);
+    traverse(head);
+    head = insertAtEnd("Suicidal by Melly", head);
+    traverse(head);
+    head = insertAtEnd("The Shade by Rex Orange County", head);
+    traverse(head);
+    head = insertAtBeginning ("Love by Keyshie cole", head);
+    traverse(head);
+    head = insertAtBeginning ("Trap Queen by Fetty Wap", head);
+    traverse(head);
+    head = insertAtBeginning ("Die Young by Kesha", head);
+    traverse(head);
+    head = insertAtBeginning ("Again by Fetty Wap", head);
+    traverse(head);
+    cout << insertAfter("Love by Keyshie cole","Marilag by Dionela", head);
+    traverse(head);
+    cout << insertAfter("Trap Queen by Fetty Wap","Yakap by Zack Tabudlo", head);
+    traverse(head);
+    cout << insertAfter("Again by Fetty Wap","Magbalik by Callalily", head);
     traverse(head);
     
-    insertAfter ("Uhaw by Dilaw", "Gods plan by Drake", head);
-    insertAfter ("Marilag by Dionela", "City Girl by Shantidobe", head);
-    insertAfter ("Palagi by KZ tandingan", "AMAZING by Rex Orange Country", head);
+    cout << deleteAtEnd(head);
+    traverse(head);
+    head = deleteFromBeginning(head);
+    traverse(head);
+    head = deleteFromGivenNode("Yakap by Zack Tabudlo",head);
     traverse(head);
     
-    
-
     return 0;
 }
